03d11844c4628c90a5659c40bf777f403ea9562c
Logan Kearsley <chronosurfer@gmail.com>
2013-08-20 14:23:56 -0600

    Bugfixes
    Finally started testing against a live server

--- Web/js/ajaxclient.js
+++ Web/js/ajaxclient.js
@@ -1,6 +1,5 @@
 var LexTermServer = (function($){
 	"use strict";
-	var proto;
 	
 	function make_request(method,target,body){
 		var def = $.Deferred();
@@ -12,47 +11,45 @@ var LexTermServer = (function($){
 			dataType: "json"
 		}).then(function(response){
 			if(false){ def.reject(problem); }
-			else{ def.resolve(Object.freeze(response.result)); }
+			else{ def.resolve(Object.freeze(response)); }
 		});
 		return def.promise();
 	}
 	
-	function Enums(base, lang){
+	function Enums(base, langCode){
 		var emap = {};
 		this.base = base;
-		this.lang = lang;
+		this.langCode = langCode;
 		this.emap = emap;
-		make_request("GET",base+"/lex/"+lang+"/enums/").then(function(enums){
+		make_request("GET",base+"/lex/"+langCode+"/enums/").then(function(enums){
 			enums.forEach(function(name){
-				make_request("GET",base+"/lex/"+lang+"/enums/"+name+"/").then(function(values){
+				make_request("GET",base+"/lex/"+langCode+"/enums/"+name+"/").then(function(values){
 					emap[name] = values;
 				});
 			});
 		});
 	}
 	
-	proto = Enums.prototype;
-	
-	proto.get = function(name){
+	Enums.prototype.get = function(name){
 		var def = $.Deferred();
 		if(name in this.emap){ def.resolve(this.emap[name]); }
 		else{ def.reject(); }
 		return def.promise();
 	};
-	proto.list = function(){
+	Enums.prototype.list = function(){
 		var def = $.Deferred();
 		def.resolve(Object.keys(this.emap));
 		return def.promise();
 	};
-	proto.addValue = function(name,body){
+	Enums.prototype.addValue = function(name,body){
 		var emap = this.emap,
-			promise = make_request("POST",this.base+"/lex/"+this.lang+"/enums/"+name+"/add/",body);
+			promise = make_request("POST",this.base+"/lex/"+this.langCode+"/enums/"+name+"/add/",body);
 		promise.then(function(values){ emap[name] = values; });
 		return promise;
 	};
-	proto.removeValue = function(name,body){
+	Enums.prototype.removeValue = function(name,body){
 		var emap = this.emap,
-			promise = make_request("POST",this.base+"/lex/"+this.lang+"/enums/"+name+"/delete/",body);
+			promise = make_request("POST",this.base+"/lex/"+this.langCode+"/enums/"+name+"/delete/",body);
 		promise.then(function(values){
 			if(!values.length){ delete emap[name]; }
 			else{ emap[name] = values; }
@@ -60,170 +57,160 @@ var LexTermServer = (function($){
 		return promise;
 	};
 	
-	function Representations(base, lang){
+	function Representations(base, langCode){
 		var that = this;
 		this.base = base;
-		this.lang = lang;
-		this.list = null;
-		make_request("GET",this.base+"/lex/"+lang+"/representations/")
-			.then(function(reps){ that.list = reps; });
+		this.langCode = langCode;
+		this.listDef = make_request("GET",this.base+"/lex/"+langCode+"/representations/");
 	}
 	
-	proto = Representations.prototype;
-	
-	proto.list = function(){
-		var def = $.Deferred();
-		def.resolve(this.list);
-		return def.promise();
+	Representations.prototype.list = function(){
+		return this.listDef;
 	};
-	proto.add = function(body){
-		var that = this,
-			promise = make_request("POST",this.base+"/lex/"+this.lang+"/representations/add/",body);
-		promise.then(function(reps){ that.list = reps; });
-		return promise;
+	Representations.prototype.add = function(body){
+		this.listDef = make_request("POST",this.base+"/lex/"+this.langCode+"/representations/add/",body);
+		return this.listDef;
 	};
-	proto.remove = function(body){		
-		var that = this,
-			promise = make_request("POST",this.base+"/lex/"+this.lang+"/representations/delete/",body);
-		promise.then(function(reps){ that.list = reps; });
-		return promise;
+	Representations.prototype.remove = function(body){
+		this.listDef = make_request("POST",this.base+"/lex/"+this.langCode+"/representations/delete/",body);
+		return this.listDef;
 	};
 	
-	function ClassList(base, lang){
+	function ClassList(base, langCode){
 		var cmap = {};
 		this.base = base;
-		this.lang = lang;
+		this.langCode = langCode;
 		this.cmap = cmap;
-		make_request("GET",base+"/lex/"+lang+"/classes").then(function(classes){
+		make_request("GET",base+"/lex/"+langCode+"/classes").then(function(classes){
 			classes.forEach(function(name){
-				make_request("GET",base+"/lex/"+lang+"/classes/"+name+"/")
+				make_request("GET",base+"/lex/"+langCode+"/classes/"+name+"/")
 					.then(function(classobj){ cmap[name] = classobj; });
 			});
 		});
 	}
 	
-	proto = ClassList.prototype;
-	
-	proto.get = function(name){
+	ClassList.prototype.get = function(name){
 		var def = $.Deferred();
 		if(name in this.cmap){ def.resolve(this.cmap[name]); }
 		else{ def.reject(); }
 		return def.promise();
 	};
 	
-	proto.list = function(){
+	ClassList.prototype.list = function(){
 		var def = $.Deferred();
 		def.resolve(Object.keys(this.cmap));
 		return def.promise();
 	};
 	
-	proto.edit = function(name,body){
+	ClassList.prototype.edit = function(name,body){
 		var cmap = this.cmap,
-			promise = make_request("POST",this.base+"/lex/"+this.lang+"/classes/"+name+"/",body);
+			promise = make_request("POST",this.base+"/lex/"+this.langCode+"/classes/"+name+"/",body);
 		promise.then(function(classobj){ cmap[name] = classobj; });
 		return promise;
 	};
 	
 	function LexTermLang(base,lang){
 		this.base = base;
-		this.lang = lang;
-		this.enums = new Enums(base,lang);
-		this.representations = new Representations(base,lang);
-		this.classes = new ClassList(base,lang);
+		this.langCode = lang.langCode;
+		this.name = lang.name;
+		this.enums = new Enums(base,lang.langCode);
+		this.representations = new Representations(base,lang.langCode);
+		this.classes = new ClassList(base,lang.langCode);
 	}
-
-	proto = LexTermLang.prototype;
 	
-	proto.listLexemes = function(){
-		return make_request("GET",this.base+"/lex/"+this.lang+"/lexemes/");
+	LexTermLang.prototype.listLexemes = function(){
+		return make_request("GET",this.base+"/lex/"+this.langCode+"/lexemes/");
 	};
-	proto.getLexeme = function(id){
-		return make_request("GET",this.base+"/lex/"+this.lang+"/lexemes/"+id+"/");
+	LexTermLang.prototype.getLexeme = function(id){
+		return make_request("GET",this.base+"/lex/"+this.langCode+"/lexemes/"+id+"/");
 	};
-	proto.getLexemeByLemma = function(lemma){
-		return make_request("GET",this.base+"/lex/"+this.lang+"/lemmas/"+lemma+"/");
+	LexTermLang.prototype.getLexemeByLemma = function(lemma){
+		return make_request("GET",this.base+"/lex/"+this.langCode+"/lemmas/"+lemma+"/");
 	};
 	
-	proto.createLexeme = function(body){
-		return make_request("POST",this.base+"/lex/"+this.lang+"/lexemes/",body);
+	LexTermLang.prototype.createLexeme = function(body){
+		return make_request("POST",this.base+"/lex/"+this.langCode+"/lexemes/",body);
 	};
-	proto.editLexeme = function(id,body){
-		return make_request("POST",this.base+"/lex/"+this.lang+"/lexemes/"+id+"/",body);
+	LexTermLang.prototype.editLexeme = function(id,body){
+		return make_request("POST",this.base+"/lex/"+this.langCode+"/lexemes/"+id+"/",body);
 	};
 	
 	function LexTermSubj(base,subj){
 		this.base = base;
 		this.subj = subj;
 	}
-
-	proto = LexTermSubj.prototype;
 	
-	proto.listConcepts = function(langs){
-		var query = (langs && langs.length)?"?lang="+langs.join(','):"";
+	LexTermSubj.prototype.listConcepts = function(lcodes){
+		var query = (lcodes && lcodes.length)?"?lang="+lcodes.join(','):"";
 		return make_request("GET",this.base+"/term/subjects/"+this.subj+"/concepts/"+query);
 	};
-	proto.getConcept = function(id,langs){
-		var query = (langs && langs.length)?"/?lang="+langs.join(','):"/";
+	LexTermSubj.prototype.getConcept = function(id,lcodes){
+		var query = (lcodes && lcodes.length)?"?lang="+lcodes.join(','):"";
 		return make_request("GET",this.base+"/term/subjects/"+this.subj+"/concepts/"+id+query);
 	};
-	proto.getConceptByTerm = function(lemma,langs){
-		var query = (langs && langs.length)?"/?lang="+langs.join(','):"/";
+	LexTermSubj.prototype.getConceptByTerm = function(lemma,lcodes){
+		var query = (lcodes && lcodes.length)?"?lang="+lcodes.join(','):"";
 		return make_request("GET",this.base+"/term/subjects/"+this.subj+"/lemmas/"+lemma+query);
 	};
 	
-	proto.createConcept = function(body){
+	LexTermSubj.prototype.createConcept = function(body){
 		return make_request("POST",this.base+"/term/subjects/"+this.subj+"/concepts/",body);
 	};
-	proto.editConcept = function(id,body){
+	LexTermSubj.prototype.editConcept = function(id,body){
 		return make_request("POST",this.base+"/term/subjects/"+this.subj+"/concepts/"+id+"/",body);
 	};
 	
 	function LexTermServer(base){
-		var lmap = {}, smap = {};
+		var lmap = {}, smap = {},
+			d1 = $.Deferred(),
+			d2 = $.Deferred();
 		this.base = base;
 		this.lmap = lmap;
 		this.smap = smap;
+		this.promise = $.when(d1.promise(), d2.promise());
 		make_request("GET",this.base+"/lex/").then(function(langs){
-			langs.forEach(function(lcode){ lmap[lcode] = new LexTermLang(base, lcode); });
+			langs.forEach(function(lang){ lmap[lang.langCode] = new LexTermLang(base, lang); });
+			d1.resolve();
 		});
 		make_request("GET",this.base+"/term/subjects/").then(function(subjs){
 			subjs.forEach(function(sname){ smap[sname] = new LexTermSubj(base, sname); });
+			d2.resolve();
 		});
 	}
 	
-	proto = LexTermServer.prototype;
+	LexTermServer.prototype.load = function(){ return this.promise; };
 
-	proto.Language = function(lang){ return this.lmap[lang]||null; };
-	proto.Subject = function(subj){ return this.smap[subj]||null; };
+	LexTermServer.prototype.Language = function(lang){ return this.lmap[lang]||null; };
+	LexTermServer.prototype.Subject = function(subj){ return this.smap[subj]||null; };
 	
-	proto.listLanguages = function(){
+	LexTermServer.prototype.listLanguages = function(){
 		var def = $.Deferred();
 		def.resolve(Object.keys(this.lmap));
 		return def.promise();
 	};
-	proto.listSubjects = function(){
+	LexTermServer.prototype.listSubjects = function(){
 		var def = $.Deferred();
 		def.resolve(Object.keys(this.smap));
 		return def.promise();
 	};
 
-	proto.listConcepts = function(langs){
-		var query = (langs && langs.length)?"?lang="+langs.join(','):"";
+	LexTermServer.prototype.listConcepts = function(lcodes){
+		var query = (lcodes && lcodes.length)?"?lang="+lcodes.join(','):"";
 		return make_request("GET",this.base+"/term/concepts/"+query);
 	};
-	proto.getConcept = function(id,langs){
-		var query = (langs && langs.length)?"/?lang="+langs.join(','):"/";
+	LexTermServer.prototype.getConcept = function(id,lcodes){
+		var query = (lcodes && lcodes.length)?"/?lang="+langs.join(','):"/";
 		return make_request("GET",this.base+"/term/concepts/"+id+query);
 	};
-	proto.getConceptByTerm = function(lemma,langs){
-		var query = (langs && langs.length)?"/?lang="+langs.join(','):"/";
+	LexTermServer.prototype.getConceptByTerm = function(lemma,lcodes){
+		var query = (lcodes && lcodes.length)?"/?lang="+langs.join(','):"/";
 		return make_request("GET",this.base+"/term/lemmas/"+lemma+query);
 	};
 	
-	proto.createConcept = function(body){
+	LexTermServer.prototype.createConcept = function(body){
 		return make_request("POST",this.base+"/term/concepts/",body);
 	};
-	proto.editConcept = function(id,body){
+	LexTermServer.prototype.editConcept = function(id,body){
 		return make_request("POST",this.base+"/term/concepts/"+id+"/",body);
 	};
 
