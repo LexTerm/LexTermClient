d2fe91b54a01b29e2b0cd9d9c3c411a6b1243699
Jason Housley <housleyjk@gmail.com>
2014-02-17 13:54:12 +0900

    its slow

--- app/scripts/routes/term.js
+++ app/scripts/routes/term.js
@@ -1,6 +1,7 @@
 Ltm.TermRoute = Ember.Route.extend({
     model: function(params) {
-        // TODO implement pagination via params
+        // TODO implement pagination
+        // TODO optimize
         var self = this;
         return self.get('store').find('collection', params.collection_id).then(
             function(collection) {
@@ -10,47 +11,58 @@ Ltm.TermRoute = Ember.Route.extend({
                       'concept', {'lexemes__collections': collection.get('id')}),
                   languages: self.get('store').find(
                       'language', {'lexical_classes__lexemes__collections': collection.get('id')}),
+                  lexicalClasses: self.get('store').find(
+                      'lexicalclass', {'lexemes__collections': collection.get('id')}),
+                  lexemes: self.get('store').find(
+                      'lexeme', {'collections': collection.get('id')}),
+                  lexicalForms: self.get('store').find(
+                      'lexicalform', {'lexeme__collections': collection.get('id')}),
                   subjectFields: self.get('store').find(
-                      'subjectfield', {'concepts__lexemes__collections': collection.get('id')})
-                  //representations: self.get('store').find(
-                      //'representation', {'lexical_form__lexeme__collections': collection.get('id')})
+                      'subjectfield', {'concepts__lexemes__collections': collection.get('id')}),
+                  representations: self.get('store').find(
+                      'representation', {'lexical_form__lexeme__collections': collection.get('id')})
                 });
             },
             function() {
                 return Ember.RSVP.hash({
                   concepts: self.get('store').find('concept'),
                   languages: self.get('store').find('language'),
-                  subjectFields: self.get('store').find('subjectfield')
-                  //representations: self.get('store').find('representation')
-                });
-            }).then(function(hash) {
-                var rep_promises = [];
-                var rep_hash = {};
-                hash.concepts.forEach(function(concept) {
-                    //TODO limit to languages used by concepts
-                    rep_hash[concept.get('id')] = rep_hash[concept.get('id')] || {concept: concept, languages: {}};
-                    hash.languages.forEach(function(lang) {
-                        var repstring = '';
-                        rep_hash[concept.get('id')].languages[lang.get('id')] = rep_hash[concept.get('id')].languages[lang.get('id')] || {language: lang, repstring: repstring};
-                        rep_promises.push(self.get('store').find(
-                            'representation', {
-                                lexical_form__lexeme__concept: concept.get('id'),
-                                lexical_form__lexeme__lexical_class__language: lang.get('id')
-                            }).then(function(reps) {
-                                reps.forEach(function(rep) {
-                                    repstring += rep.get('name') + "\n"; 
-                                }); 
-                                return reps;
-                            })
-                        );
-                    });
-                });
-
-                return Ember.RSVP.all(rep_promises).then(function(replist) {
-                    hash.representations = rep_hash; //not replist
-                    return hash; 
+                  lexicalClasses: self.get('store').find('lexicalclass'),
+                  lexemes: self.get('store').find('lexeme'),
+                  lexicalForms: self.get('store').find('lexicalform'),
+                  subjectFields: self.get('store').find('subjectfield'),
+                  representations: self.get('store').find('representation')
                 });
             });
+
+        //.then(function(hash) {
+                //var rep_promises = [];
+                //var rep_hash = {};
+                //hash.concepts.forEach(function(concept) {
+                    ////TODO limit to languages used by concepts
+                    //rep_hash[concept.get('id')] = rep_hash[concept.get('id')] || {concept: concept, languages: {}};
+                    //hash.languages.forEach(function(lang) {
+                        //var repstring = '';
+                        //rep_hash[concept.get('id')].languages[lang.get('id')] = rep_hash[concept.get('id')].languages[lang.get('id')] || {language: lang, repstring: repstring};
+                        //rep_promises.push(self.get('store').find(
+                            //'representation', {
+                                //lexical_form__lexeme__concept: concept.get('id'),
+                                //lexical_form__lexeme__lexical_class__language: lang.get('id')
+                            //}).then(function(reps) {
+                                //reps.forEach(function(rep) {
+                                    //repstring += rep.get('name') + "\n";
+                                //});
+                                //return reps;
+                            //})
+                        //);
+                    //});
+                //});
+
+                //return Ember.RSVP.all(rep_promises).then(function(replist) {
+                    //hash.representations = rep_hash; //not replist
+                    //return hash;
+                //});
+            //});
     },
 
     renderTemplate: function() {
