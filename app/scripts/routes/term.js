e40f87cf4d1fbf02cf09a2be844cf3bff0cb9b29
Jason Housley <housleyjk@gmail.com>
2014-02-20 23:50:23 +0900

    Navigation working.

--- app/scripts/routes/term.js
+++ app/scripts/routes/term.js
@@ -1,57 +1,12 @@
 Ltm.TermRoute = Ember.Route.extend({
     model: function(params) {
-        var self = this;
-        return self.get('store').find('collection', params.collection_id).then(
-            function(collection) {
-                return Ember.RSVP.hash({
-                  collection: collection,
-                  concepts: self.get('store').find(
-                      'concept', {'lexemes__collections': collection.get('id')}),
-                  languages: self.get('store').find(
-                      'language', {'lexical_classes__lexemes__collections': collection.get('id')}),
-                  lexicalClasses: self.get('store').find(
-                      'lexicalclass', {'lexemes__collections': collection.get('id')}),
-                  lexemes: self.get('store').find(
-                      'lexeme', {'collections': collection.get('id')}),
-                  lexicalForms: self.get('store').find(
-                      'lexicalform', {'lexeme__collections': collection.get('id')}),
-                  subjectFields: self.get('store').find(
-                      'subjectfield', {'concepts__lexemes__collections': collection.get('id')}),
-                  representations: self.get('store').find(
-                      'representation', {'lexical_form__lexeme__collections': collection.get('id')})
-                });
+        return this.get('store').find('collection', params.collection_id).then(
+              function(collection) {
+                return {collection: collection, collection_id: collection.get('id')};
             },
-            function () {
-              return false;
+              function() {
+                return {collection_id: 'all'};
             });
-    },
-
-    setupController: function(controller, model) {
-      console.log('called yeah');
-      var self = this;
-      Ember.RSVP.Promise.cast(model).then(
-          function(model) {
-            console.log('got model');
-            if (model) {
-              console.log('returning');
-              console.log(model);
-              return model;
-            } else {
-              console.log('returning hash');
-              return Ember.RSVP.hash({
-                concepts: self.get('store').find('concept'),
-                languages: self.get('store').find('language'),
-                lexicalClasses: self.get('store').find('lexicalclass'),
-                lexemes: self.get('store').find('lexeme'),
-                lexicalForms: self.get('store').find('lexicalform'),
-                subjectFields: self.get('store').find('subjectfield'),
-                representations: self.get('store').find('representation')
-              }).then(function(model) {
-            console.log('setting model');
-            controller.set('model', model);
-        });
-            }
-        });
     },
 
     renderTemplate: function() {
