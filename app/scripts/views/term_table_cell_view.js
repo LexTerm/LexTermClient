4bc390a126b5195eb8b63e3430baf59245e26209
Jason Housley <housleyjk@gmail.com>
2014-02-28 11:08:58 +0900

    Modular and simple.

---
+++ app/scripts/views/term_table_cell_view.js
@@ -0,0 +1,38 @@
+Ltm.TermTableCellView = Ember.Table.TableCell.extend({
+  templateName: 'term_table_cell',
+  //destination: function(){
+    //var column = this.get('column'),
+        //row    = this.get('row');
+    //if (column && row) {
+      //var content = this.get('cellContent');
+      //return content.destination;
+    //} else {
+      ////default to entry
+      //return 'entry';
+    //}
+  //}.property('column', 'row', 'cellContent'),
+
+  //instance: function(){
+    //var column = this.get('column'),
+        //row    = this.get('row');
+    //if (column && row) {
+      //var content = this.get('cellContent');
+      //return content.instance;
+    //} else {
+      //return;
+    //}
+
+  //}.property('column', 'row', 'cellContent'),
+
+  //inner: function(){
+    //var column = this.get('column'),
+        //row    = this.get('row');
+    //if (column && row) {
+      //var content = this.get('cellContent');
+      //return content.inner;
+    //} else {
+      //return 'Loading...';
+    //}
+  //}.property('column', 'row', 'cellContent')
+
+});
